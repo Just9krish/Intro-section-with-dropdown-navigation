ad2e3c6ff564a5b56bdf65b636453eb14eb15948
just9krish <just9krish434@gamil.com>
2022-06-23 23:56:51 +0530

    made mobile navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,12 @@
 const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
 const navigation = document.querySelector(".navigation");
 const body = document.querySelector("body");
+const featureArrow = document.querySelector(".feature-arrow-img");
+const companyArrow = document.querySelector(".comapany-arrow-img");
+const featureToggle = document.querySelector(".feature-toggle");
+const companyToggle = document.querySelector(".company-toggle");
+const submenu = document.querySelector(".submenu");
+const companySubmenu = document.querySelector(".company-submenu");
 
 mobileNavToggle.addEventListener("click", () => {
   const visibility = navigation.getAttribute("data-visible");
@@ -15,3 +21,30 @@ mobileNavToggle.addEventListener("click", () => {
     body.style.overflow = "auto";
   }
 });
+
+
+featureToggle.addEventListener ("click", () => {
+    const visibility = submenu.getAttribute("data-visible")
+    featureArrow.classList.toggle('rotate')
+
+    if (visibility === "false") {
+        submenu.setAttribute("data-visible", true);
+        featureToggle.setAttribute("aria-expanded", true)
+      } else {
+        submenu.setAttribute("data-visible", false);
+        featureToggle.setAttribute("aria-expanded", false);
+      }
+})
+
+companyToggle.addEventListener ("click", () => {
+    const visibility = companySubmenu.getAttribute("data-visible")
+    companyArrow.classList.toggle('rotate')
+
+    if (visibility === "false") {
+        companySubmenu.setAttribute("data-visible", true);
+        companyToggle.setAttribute("aria-expanded", true)
+      } else {
+        companySubmenu.setAttribute("data-visible", false);
+        companyToggle.setAttribute("aria-expanded", false);
+      }
+})
